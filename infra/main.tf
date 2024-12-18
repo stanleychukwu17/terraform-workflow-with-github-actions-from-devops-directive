1fea79de7709e2c8751980c1b0e376093f533521
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-12-18 15:21:37 +0100

    added aws_vpc resource & the aws_secret_key

--- infra/main.tf
+++ infra/main.tf
@@ -8,7 +8,17 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
-  access_key = ""
-  secret_key = ""
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${local.environment}-${local.common-tags["Project"]}-VPC"
+  }
 }
